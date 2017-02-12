e307f17bde3c328abf0bc372939b7258172100b7
Chris Camaratta <chris.camaratta@hotmail.com>
2017-02-12 11:18:33 -0800

    Listen to env port

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(80, function(){
-    console.log('Server running on 80...');
+var port = process.env.PORT || 8080;
+
+connect().use(serveStatic(__dirname)).listen(port, function(){
+  console.log('Listening on ' + port);
 });
